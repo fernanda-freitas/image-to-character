828cd1edf92981cdb36139ec88f9de9dc02ce9e8
Fernanda Freitas <idea.fernanda@gmail.com>
2026-06-30 09:50:24 +0100

    config deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,12 @@
+const isGithubPages = process.env.GITHUB_ACTIONS === "true";
+const repoName = "image-to-character";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  output: "export",
+  images: { unoptimized: true },
+  basePath: isGithubPages ? `/${repoName}` : "",
+  assetPrefix: isGithubPages ? `/${repoName}/` : "",
 };
 
 export default nextConfig;
